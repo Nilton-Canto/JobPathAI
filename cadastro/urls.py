82bce886d513fb2cb42eac7463776af7454166c1
Henrique Ribeiro Souza <61210736+HenriqueRSouza@users.noreply.github.com>
2025-08-30 22:41:08 -0300

    Revert "Implementação da feature de login"

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from .views import LoginView  # Importa a view de login
 
 urlpatterns = [
     path("", views.index, name="index"),  # Maps the root URL to the index
-        path('login/', LoginView.as_view(), name='login'),  # Rota para a página de login
 ]
