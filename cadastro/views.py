7ea1675af0021fe12cbe89c433ceaf25e96e24df
Theo Casella <theocasella@gmail.com>
2025-08-31 01:25:53 -0300

    Feature de novos usuarios.
    Usei nome, email, idade e CPF.
    
    Implementei uma verificação de email e cpf para não ter usuarios com essas informações repetidas.

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,7 @@
 from django.views import View  # Importa a classe base para views orientadas a objetos
 from django.contrib.auth import authenticate, login  # Funções para autenticar e logar usuários
 from django.shortcuts import render, redirect  # Funções para renderizar templates e redirecionar
+from .models import Cadastro  # Importa o modelo Cadastro
 
 # Create your views here.
 def index(request):
@@ -24,3 +25,38 @@ class LoginView(View):
         else:
             # Se falhar, renderiza o template novamente com uma mensagem de erro
             return render(request, 'cadastro/login.html', {'error': 'Usuário ou senha inválidos'})
+
+def novo_usuario(request):
+    if request.method == 'POST':
+        # Recebe os dados do formulário enviados via POST
+        nome_recebido = request.POST.get('nome')
+        email_recebido = request.POST.get('email')
+        idade_recebida = request.POST.get('idade')
+        cpf_recebido = request.POST.get('cpf')
+
+        # Lógica de verificação
+        usuario_existente = Cadastro.objects.filter(email=email_recebido).exists()
+        cpf_existente = Cadastro.objects.filter(cpf=cpf_recebido).exists()
+
+        if usuario_existente:
+            # Se o e-mail já existe, mostre uma mensagem de erro
+            contexto = {'erro': 'Este e-mail já está cadastrado.'}
+            return render(request, 'cadastro/novo_usuario.html', contexto)
+
+        if cpf_existente:
+            # Se o CPF já existe, mostre uma mensagem de erro
+            contexto = {'erro': 'Este CPF já está cadastrado.'}
+            return render(request, 'cadastro/novo_usuario.html', contexto)
+        
+        # Cria um novo objeto "Cadastro" e salva no banco de dados
+        novo_cadastro = Cadastro.objects.create(
+            nome=nome_recebido,
+            email=email_recebido,
+            idade=idade_recebida,
+            cpf=cpf_recebido,
+        )
+
+        # Redireciona para a página de login após o cadastro
+        return redirect('login')
+    
+    return render(request, 'cadastro/novo_usuario.html') # Vai para a página de cadastro de novo usuário
