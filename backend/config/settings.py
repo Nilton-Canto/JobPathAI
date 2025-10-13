20dc1e1b30d17b6f366b33f8c98a24737ac71b06
HenriqueRSouza <henriqueikesouza@gmail.com>
2025-10-13 18:53:07 -0300

    fix: add CI workflow

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "corsheaders.middleware.CorsMiddleware", # Mover para cima, antes de SessionMiddleware e CsrfViewMiddleware
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
